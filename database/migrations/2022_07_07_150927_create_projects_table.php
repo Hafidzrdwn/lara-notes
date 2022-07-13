1fccb438e43f65389ec5259761fedc9669ad7023
Hafidzrdwn <hafidz10032005@gmail.com>
2022-07-14 01:48:52 +0700

    change migrations on projects and notes

--- database/migrations/2022_07_07_150927_create_projects_table.php
+++ database/migrations/2022_07_07_150927_create_projects_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('workspace_id')->onDelete('cascade');
+            $table->foreignId('workspace_id')->constrained()->onDelete('cascade');
             $table->foreignId('category_id');
             $table->string('title');
             $table->string('slug')->unique();
