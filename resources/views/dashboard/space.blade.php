11874689a259bd06d234bf73b2cd8d8112fb68d9
Hafidzrdwn <hafidz10032005@gmail.com>
2022-07-22 23:43:20 +0700

    add margin bottom in space dashboard page

--- resources/views/dashboard/space.blade.php
+++ resources/views/dashboard/space.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard', ['title' => 'Your Workspaces'])
 
 @section('content')
-<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 mb-5">
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-4 border-bottom">
     <h1 class="h2">Your Workspaces</h1>
   </div>
